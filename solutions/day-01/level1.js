8ef351fae34f1deded07c9e2f54691ad002cc64b
Saverio Perrone <saverio.perrone20@gmail.com>
2024-05-11 23:20:11 +0200

    day 1 solution

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,31 @@
+//comments can make code readable
+//welcome to 30DaysOfJavaScript
+/*
+comnments can make code readable, easy to reuse and informative
+*/
+let string = 'string'
+let bool = true
+let undef = undefined
+let nullVariable = null
+console.log(typeof string)
+console.log(typeof bool)
+console.log(typeof undef)
+console.log(typeof nullVariable)
+let var1
+let var2
+let var3
+let var4
+let var5 = false
+let var6 = 4.23
+let var7 = 'glig'
+let var8 =  1
+let firstName = 'John'
+let lastName = 'Silver'
+let maritalStatus = false
+let country = 'Germany'
+let age = 25
+let fName = 'John', lName = 'Silver', mStatus = false, cTry = 'Germany', aGe = 25
+let myAge = 24
+let yourAge = 35
+console.log('I am', myAge, 'years old')
+console.log('You are', yourAge, 'years old')
