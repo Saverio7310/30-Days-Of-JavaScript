a40d4c19da32b99eeabada729dcba9c5553baf62
Saverio Perrone <saverio.perrone20@gmail.com>
2024-05-29 22:05:19 +0200

    day 4 solution 1

---
+++ solutions/day-04/level1.js
@@ -0,0 +1,14 @@
+const caseValue = 'a'
+switch (caseValue) {
+    case 'a':
+        console.log('a')
+    case 'b':
+        console.log('b')
+        break
+    case 'c':
+        console.log('c')
+    case 'd':
+        console.log('d')
+    default:
+        console.log('default')
+}
