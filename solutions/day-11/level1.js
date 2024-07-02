c3c45df1fc4952ae58dc6d7aab64ee383bef629e
Saverio Perrone <saverio.perrone20@gmail.com>
2024-07-02 20:07:44 +0200

    day 11 solution 1

---
+++ solutions/day-11/level1.js
@@ -0,0 +1,18 @@
+const constants = [2.72, 3.14, 9.81, 37, 100]
+
+const [e, pi, acc, temp, limit] = constants
+
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+
+const [fin, est, sw, den, nor] = countries
+
+const rectangle = {
+    width: 20,
+    height: 10,
+    area: 200,
+    perimeter: 60
+}
+
+const {width, height, area, perimeter} = rectangle
+
+console.log(e, pi, acc, temp, limit, fin, est, sw, den, nor, width, height, area, perimeter);
