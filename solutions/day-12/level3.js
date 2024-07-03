4105e1fbec407f626d79c97e4b4d266855c61032
Saverio Perrone <saverio.perrone20@gmail.com>
2024-07-03 18:15:56 +0200

    day 12 solution 3

---
+++ solutions/day-12/level3.js
@@ -0,0 +1,11 @@
+sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+/**
+ * 
+ * @param {string} str 
+ * @returns 
+ */
+function cleanText(str){
+    return str.replace(/[$%@&#;]/g, '')
+}
+
+console.log(cleanText(sentence))
